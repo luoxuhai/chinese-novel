f219b6de540410114ba149c7070ea37d0bd274fa
ibyli <15186743693@163.com>
2019-08-27 20:24:09 +0800

    第一版新增图书359

---
+++ app/controller/oss_to_local.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const fs = require('fs');
+
+module.exports = class OssToLocalController extends Controller {
+  async index() {
+    const { ctx } = this;
+
+    // 本地圖書總數
+    let localCount = 0;
+    const typeDir = fs.readdirSync(`${process.cwd()}/resources`);
+
+    typeDir.forEach(e => {
+      localCount += fs.readdirSync(`${process.cwd()}/resources/${e}`).length;
+    });
+
+    const addCount = await ctx.service.ossToLocal.index(localCount);
+
+    ctx.body = `新增${addCount}`;
+  }
+};
---
+++ app/service/oss_to_local.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Service = require('egg').Service;
+const fs = require('fs');
+
+module.exports = class OssToLocalService extends Service {
+  async index(localCount) {
+    const { ctx } = this;
+
+    const books = await ctx.model.Book.find({}, [
+      'bookName',
+      'bookDesc',
+      'bookType',
+      'author',
+      'wordSum',
+      'catalogueList',
+      'catalogueSum',
+      'bookFile',
+    ])
+      .populate('author', [ 'name', 'dynasty', 'intro' ])
+      // 跳過本地已有圖書
+      .skip(localCount);
+
+    for (const [ i, e ] of books.entries()) {
+      console.log(`開始緩存 ${e.bookName}`);
+      const localPath = `${process.cwd()}/resources/${e.bookType || '其他'}/${
+        e.bookName
+      }`;
+
+      fs.mkdirSync(localPath, {
+        recursive: true,
+      });
+
+      for (const [ _i ] of e.catalogueList.entries()) {
+        await ctx.oss.get(
+          `${e.bookFile}/${_i}.html`,
+          `${localPath}/${_i}.html`
+        );
+      }
+
+      // 生成圖書資訊JSON檔案
+      fs.writeFileSync(
+        `${localPath}/info.json`,
+        JSON.stringify(
+          {
+            name: e.bookName,
+            catalogues: e.catalogueList,
+            bookType: e.bookType,
+            author: e.author,
+            words: e.wordSum,
+            catalogueTotal: e.catalogueSum,
+          },
+          (key, value) => {
+            if (key === 'author') {
+              delete value._id;
+              return value;
+            }
+            return value;
+          },
+          2
+        )
+      );
+      // if (i === 0) break;
+      console.log('緩存完成!');
+    }
+
+    return books.length;
+  }
+};
